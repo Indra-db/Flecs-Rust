a5ac394d2a621630ee6e2c1628d9ec050f092e32
Indradb <60851042+Indra-db@users.noreply.github.com>
2023-09-08 18:07:59 +0100

    add generic get_id and get_id_pair to world with the new cached component data

--- src/core/world.rs
+++ src/core/world.rs
@@ -1,5 +1,7 @@
 use super::c_binding::bindings::ecs_init;
 use super::c_types::WorldT;
+use super::component::CachedComponentData;
+use super::id::Id;
 
 pub struct World {
     pub world: *mut WorldT,
@@ -14,5 +16,15 @@ impl World {
 
     pub fn new_from_world(world: *mut WorldT) -> Self {
         Self { world }
+    }
+
+    /// Get id from a type.
+    fn get_id<T: CachedComponentData>(&self) -> Id {
+        Id::new(self.world, T::get_id())
+    }
+
+    /// get pair id from relationship, object.
+    fn get_id_pair<T: CachedComponentData, U: CachedComponentData>(&self) -> Id {
+        Id::new_world_pair(self.world, T::get_id(), U::get_id())
     }
 }
