4fc78d3f4d94e7e03fc4221328802552c8fffd69
Indradb <60851042+Indra-db@users.noreply.github.com>
2024-02-28 20:14:52 +0700

    SystemRunnerFluent and complete System impl

--- flecs_ecs/src/addons/system/system.rs
+++ flecs_ecs/src/addons/system/system.rs
@@ -2,12 +2,92 @@ use std::{ops::Deref, os::raw::c_void};
 
 use crate::core::{
     c_binding::bindings::{
-        ecs_get_system_ctx, ecs_os_api, ecs_system_desc_t, ecs_system_get_query, ecs_system_init,
+        ecs_get_system_ctx, ecs_os_api, ecs_run_w_filter, ecs_run_worker, ecs_system_desc_t,
+        ecs_system_get_query, ecs_system_init,
     },
+    c_types::{EntityT, TickSource},
     entity::Entity,
+    id::Id,
     query::Query,
+    utility::types::FTime,
     world::World,
 };
+
+pub struct SystemRunnerFluent {
+    stage: World,
+    id: EntityT,
+    stage_current: i32,
+    stage_count: i32,
+    offset: i32,
+    limit: i32,
+    delta_time: FTime,
+    param: *mut c_void,
+}
+
+impl SystemRunnerFluent {
+    pub fn new(
+        world: &World,
+        id: EntityT,
+        stage_current: i32,
+        stage_count: i32,
+        delta_time: FTime,
+        param: *mut c_void,
+    ) -> Self {
+        Self {
+            stage: world.clone(),
+            id,
+            stage_current,
+            stage_count,
+            offset: 0,
+            limit: 0,
+            delta_time,
+            param,
+        }
+    }
+
+    pub fn offset(&mut self, offset: i32) -> &mut Self {
+        self.offset = offset;
+        self
+    }
+
+    pub fn limit(&mut self, limit: i32) -> &mut Self {
+        self.limit = limit;
+        self
+    }
+
+    pub fn stage(&mut self, stage: &mut World) -> &mut Self {
+        self.stage = stage.clone();
+        self
+    }
+}
+
+impl Drop for SystemRunnerFluent {
+    fn drop(&mut self) {
+        if self.stage_count != 0 {
+            unsafe {
+                ecs_run_worker(
+                    self.stage.raw_world,
+                    self.id,
+                    self.stage_current,
+                    self.stage_count,
+                    self.delta_time,
+                    self.param,
+                );
+            }
+        } else {
+            unsafe {
+                ecs_run_w_filter(
+                    self.stage.raw_world,
+                    self.id,
+                    self.delta_time,
+                    self.offset,
+                    self.limit,
+                    self.param,
+                );
+            }
+        }
+    }
+}
 
 //todo!() should implement copy?
 #[derive(Clone)]
@@ -51,6 +131,18 @@ impl System {
         }
     }
 
+    pub fn new_from_existing(world: &World, system_entity: Entity) -> Self {
+        Self {
+            world: world.clone(),
+            entity: system_entity,
+        }
+    }
+
+    pub(crate) fn system_init(&mut self, world: &World) -> &mut Self {
+        world.component_named::<TickSource>("flecs::system::TickSource");
+        self
+    }
+
     pub fn set_context(&mut self, context: *mut c_void) {
         let mut desc: ecs_system_desc_t = Default::default();
         desc.entity = self.raw_id;
@@ -68,5 +160,26 @@ impl System {
         Query::<()>::new_ownership(&self.world, unsafe {
             ecs_system_get_query(self.world.raw_world, self.raw_id)
         })
+    }
+
+    pub fn run(&self, delta_time: FTime, param: *mut c_void) -> SystemRunnerFluent {
+        SystemRunnerFluent::new(&self.world, self.raw_id, 0, 0, delta_time, param)
+    }
+
+    pub fn run_worker(
+        &self,
+        stage_current: i32,
+        stage_count: i32,
+        delta_time: FTime,
+        param: *mut c_void,
+    ) -> SystemRunnerFluent {
+        SystemRunnerFluent::new(
+            &self.world,
+            self.raw_id,
+            stage_current,
+            stage_count,
+            delta_time,
+            param,
+        )
     }
 }
