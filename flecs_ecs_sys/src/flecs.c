560b595977623b3da4a6344f8c863faeb4470466
Indradb <60851042+Indra-db@users.noreply.github.com>
2024-06-26 13:37:42 +0900

    flecs core update - fix bug with `with` relationships

--- flecs_ecs_sys/src/flecs.c
+++ flecs_ecs_sys/src/flecs.c
@@ -9572,6 +9572,10 @@ void flecs_cmd_batch_for_entity(
     ecs_table_diff_t table_diff; /* Keep track of diff for observers/hooks */
     int32_t cur = start;
     ecs_id_t id;
+
+    /* Mask to let observable implementation know which components were set.
+     * This prevents the code from overwriting components with an override if
+     * the batch also contains an IsA pair. */
     ecs_flags64_t set_mask = 0;
 
     do {
@@ -9657,22 +9661,23 @@ void flecs_cmd_batch_for_entity(
             uint8_t added_index = flecs_ito(uint8_t, diff->added.count);
 
             table = flecs_find_table_add(world, table, id, diff);
-            if (diff->added.count == added_index) {
-                /* Nothing was added to the array, which means the id was added 
-                 * as result of another id (a With relationship or auto 
-                 * override). */
+
+            if (diff->added.count == (added_index + 1)) {
+                /* Single id was added, must be at the end of the array */
+                ecs_assert(ids[added_index] == id, ECS_INTERNAL_ERROR, NULL);
+                set_mask |= (1llu << added_index);
+            } else {
+                /* Id was already added or multiple ids got added. Do a linear
+                 * search to find the index we need to set the set_mask. */
                 int32_t i;
-                for (i = 0; i < added_index; i ++) {
+                for (i = 0; i < diff->added.count; i ++) {
                     if (ids[i] == id) {
                         break;
                     }
                 }
-                ecs_assert(i != added_index, ECS_INTERNAL_ERROR, NULL);
+
+                ecs_assert(i != diff->added.count, ECS_INTERNAL_ERROR, NULL);
                 set_mask |= (1llu << i);
-            } else {
-                /* Id was added, index must be at the end of the array */
-                ecs_assert(ids[added_index] == id, ECS_INTERNAL_ERROR, NULL);
-                set_mask |= (1llu << added_index);
             }
 
             world->info.cmd.batched_command_count ++;
@@ -62532,8 +62537,24 @@ void FlecsWorldSummaryImport(
 #endif
     const ecs_world_info_t *info = ecs_get_world_info(world);
 
-    ECS_SYSTEM(world, UpdateWorldSummary, EcsPreFrame, WorldSummary);
-    ECS_OBSERVER(world, OnSetWorldSummary, EcsOnSet, WorldSummary);
+    ecs_system(world, {
+        .entity = ecs_entity(world, { 
+            .name = "UpdateWorldSummary",
+            .add = ecs_ids(ecs_pair(EcsDependsOn, EcsPreFrame))
+        }),
+        .query.terms = {{ .id = ecs_id(EcsWorldSummary) }},
+        .callback = UpdateWorldSummary
+    });
+
+    ecs_observer(world, {
+        .entity = ecs_entity(world, { 
+            .name = "OnSetWorldSummary"
+        }),
+        .events = { EcsOnSet },
+        .query.terms = {{ .id = ecs_id(EcsWorldSummary) }},
+        .callback = OnSetWorldSummary
+    });
+
     ecs_set(world, EcsWorld, EcsWorldSummary, {
         .target_fps = (double)info->target_fps,
         .time_scale = (double)info->time_scale
@@ -73611,6 +73632,7 @@ bool flecs_query_trivial_test_w_wildcards(
 
     return result;
 }
+
 
 
 ///////////////////////////////////////////////////////////////
