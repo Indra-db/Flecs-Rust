b54b086861457b1eb10b44f1e1f7a1ad0518a46c
Bruce Mitchener <bruce.mitchener@gmail.com>
2024-07-03 12:28:12 +0700

    Fix build with `--no-default-features`

--- flecs_ecs/src/prelude.rs
+++ flecs_ecs/src/prelude.rs
@@ -1,4 +1,5 @@
-pub use crate::addons::module::*;
+#[cfg(feature = "flecs_module")]
+pub use crate::addons::module::Module;
 pub use crate::addons::*;
 pub use crate::core::*;
 pub use crate::macros::*;
