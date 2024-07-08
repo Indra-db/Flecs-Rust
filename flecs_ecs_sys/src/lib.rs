96cf6da6c64e1b052defcb725def85c34012fed3
Bruce Mitchener <bruce.mitchener@gmail.com>
2024-07-08 14:48:01 +0700

    flecs_ecs_sys: Flatten structure / docs. (#134)

--- flecs_ecs_sys/src/lib.rs
+++ flecs_ecs_sys/src/lib.rs
@@ -1,11 +1,17 @@
+//! Raw FFI bindings to the [`flecs`] library.
+//!
+//! These are only intended for use by the higher level bindings that build atop these.
+//!
+//! [`flecs`]: https://www.flecs.dev/
+
 #[cfg(all(feature = "force_build_release", feature = "force_build_debug"))]
 compile_error!(
     "Features 'force_build_release' and 'force_build_debug' cannot be enabled at the same time."
 );
 
-pub mod bindings;
-pub mod extensions;
-pub mod mbindings;
+mod bindings;
+mod extensions;
+mod mbindings;
 
 pub use bindings::*;
 pub use mbindings::*;
