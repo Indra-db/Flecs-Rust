5089be23b7d97513f637b11ef432b06b105048a4
Indradb <60851042+Indra-db@users.noreply.github.com>
2025-02-18 13:47:19 +0100

    feat(mdbook): add flecs img

--- flecs_ecs/doc/flecs/book/toc.js
+++ flecs_ecs/doc/flecs/book/toc.js
@@ -8,7 +8,7 @@ class MDBookSidebarScrollbox extends HTMLElement {
         super();
     }
     connectedCallback() {
-        this.innerHTML = '<ol class="chapter"><li class="chapter-item expanded "><a href="chapter_1.html"><strong aria-hidden="true">1.</strong> Chapter 1</a></li></ol>';
+        this.innerHTML = '<ol class="chapter"><li class="chapter-item expanded "><a href="quickstart.html"><strong aria-hidden="true">1.</strong> quickstart</a></li></ol>';
         // Set the current, active page, and reveal it if it's hidden
         let current_page = document.location.href.toString().split("#")[0];
         if (current_page.endsWith("/")) {
