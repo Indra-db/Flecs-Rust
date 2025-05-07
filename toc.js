3591468a1d23ee197c53b9418d4a3659b174c697
Indra-db <Indra-db@users.noreply.github.com>
2025-05-07 09:40:38 +0000

    deploy: 830cbf7dae172a8097c0e9753fb73441fd9b9386

--- toc.js
+++ toc.js
@@ -10,7 +10,7 @@ class MDBookSidebarScrollbox extends HTMLElement {
     connectedCallback() {
         this.innerHTML = '<ol class="chapter"><li class="chapter-item expanded "><a href="quickstart.html"><strong aria-hidden="true">1.</strong> quickstart</a></li></ol>';
         // Set the current, active page, and reveal it if it's hidden
-        let current_page = document.location.href.toString().split("#")[0];
+        let current_page = document.location.href.toString().split("#")[0].split("?")[0];
         if (current_page.endsWith("/")) {
             current_page += "index.html";
         }
