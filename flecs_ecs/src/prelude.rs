70532d8cb1b05477a986352c3c001a283d20222c
Indradb <60851042+Indra-db@users.noreply.github.com>
2025-09-26 15:16:24 +0900

    include Timer addon in prelude

--- flecs_ecs/src/prelude.rs
+++ flecs_ecs/src/prelude.rs
@@ -2,6 +2,8 @@
 pub use crate::addons::module::Module;
 pub use crate::addons::*;
 pub use crate::core::*;
+#[cfg(feature = "flecs_timer")]
+pub use flecs_ecs::addons::timer::TimerAPI;
 pub use flecs_ecs_derive::*;
 pub use flecs_ecs_sys::EcsComponent;
 
