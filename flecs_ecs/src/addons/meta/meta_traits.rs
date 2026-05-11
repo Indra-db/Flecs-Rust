ed5dc58839cd52f59ab06d83de09629e72260a62
Dustin Brown <me@dubrowgn.com>
2026-05-11 07:16:17 -0600

    Make MetaMember::name 'a instead of 'static
    UntypedComponent::member() and friends take a MetaMember with a 'static
    name string. This string is then immediately copied into a c-string
    before use, so the 'static lifetime is not necessary. The name only
    needs to lives for the duration of the function call.
    
    Using 'static is fine for many cases, but makes makes generating
    arbitrary component members at runtime problematic. Given the nature of
    the API, this usecase is highly likely.
    
    Instead of using 'static name, use 'a instead, such that generating
    dynamic component members at runtime using dynamic string names is
    easily possible.

--- flecs_ecs/src/addons/meta/meta_traits.rs
+++ flecs_ecs/src/addons/meta/meta_traits.rs
@@ -2,23 +2,23 @@ use super::Count;
 
 /// Meant to be used with `.member` method of (untyped) components.
 /// This is to allow an unified function API to allow arbitrary number of arguments.
-/// valid options are (name : &'static str,), (name: &'static str, count: i32), (name: &'static str, count: i32, offset: i32)
-pub trait MetaMember: 'static {
+/// valid options are (name: &'a str,), (name: &'a str, count: i32), (name: &'a str, count: i32, offset: i32)
+pub trait MetaMember<'a> {
     /// Whether to use explicit offset or not
     /// when false, flecs calculates the offset for us, this is useful for simple structs.
     /// but might fail for more complex structs.
     const USE_OFFSET: bool;
 
-    fn name(&self) -> &str;
+    fn name(&self) -> &'a str;
     fn count(&self) -> i32;
     fn offset(&self) -> i32;
 }
 
-impl MetaMember for &'static str {
+impl<'a> MetaMember<'a> for &'a str {
     const USE_OFFSET: bool = false;
 
     #[inline(always)]
-    fn name(&self) -> &str {
+    fn name(&self) -> &'a str {
         self
     }
 
@@ -33,11 +33,11 @@ impl MetaMember for &'static str {
     }
 }
 
-impl MetaMember for (&'static str,) {
+impl<'a> MetaMember<'a> for (&'a str,) {
     const USE_OFFSET: bool = false;
 
     #[inline(always)]
-    fn name(&self) -> &str {
+    fn name(&self) -> &'a str {
         self.0
     }
 
@@ -52,11 +52,11 @@ impl MetaMember for (&'static str,) {
     }
 }
 
-impl MetaMember for (&'static str, Count) {
+impl<'a> MetaMember<'a> for (&'a str, Count) {
     const USE_OFFSET: bool = false;
 
     #[inline(always)]
-    fn name(&self) -> &str {
+    fn name(&self) -> &'a str {
         self.0
     }
 
@@ -71,11 +71,11 @@ impl MetaMember for (&'static str, Count) {
     }
 }
 
-impl MetaMember for (&'static str, Count, usize) {
+impl<'a> MetaMember<'a> for (&'a str, Count, usize) {
     const USE_OFFSET: bool = true;
 
     #[inline(always)]
-    fn name(&self) -> &str {
+    fn name(&self) -> &'a str {
         self.0
     }
 
